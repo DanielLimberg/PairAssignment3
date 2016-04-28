25e2598d7436d2372440a331fd9df61b3d84a81b
DanielLimberg <d.limberg@alumni.maastrichtuniversity.nl>
2016-04-28 15:08:34 +0200

    getting rid of NA's in ECB values

--- PairAssignment3_Code/PairAssignment3_Variables_LagsPercChanges.R
+++ PairAssignment3_Code/PairAssignment3_Variables_LagsPercChanges.R
@@ -19,6 +19,17 @@ merge10$iso3c.y <- NULL
 merge10$V4 <- NULL
 merge10$V5 <- NULL
 
+# define year and Date as numeric
+merge10$year <- as.factor(merge10$year)
+merge10$Date <- gsub("Q",".",merge10$Date)
+merge10$Date <- as.numeric(merge10$Date)
+
+sel <- which(merge10$Date>1998.4) 
+merge10$ECB.MRO.change[sel][is.na(merge10$ECB.MRO.change[sel])] <- 0
+merge10$ECB.dep.change[sel][is.na(merge10$ECB.dep.change[sel])] <- 0
+
+rm(sel)
+
 # creating time lags
 # lag variables by one time period (one quarter year)
 
@@ -32,9 +43,5 @@ for (i in var) {
 
 rm(i, var)
 
-# define year and Date as numeric
-merge10$year <- as.factor(merge10$year)
-merge10$Date <- gsub("Q",".",merge10$Date)
-merge10$Date <- as.numeric(merge10$Date)
 
 merge10 <- merge10[ ,c(1,3,2,4:64)]
